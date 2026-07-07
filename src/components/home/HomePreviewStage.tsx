47bbfaf97917e77b3c4f2ecd165c5ed4ee98352d
Tom van Biene <tomjo118735@gmail.com>
2026-07-07 22:46:50 +0000

    Homepage tagline nudged up a bit

--- src/components/home/HomePreviewStage.tsx
+++ src/components/home/HomePreviewStage.tsx
@@ -23,7 +23,7 @@ export function HomePreviewStage({
   }, [])
 
   return (
-    <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden px-6 text-center" style={{ paddingTop: '3.6vh', paddingBottom: 124 }}>
+    <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden px-6 text-center" style={{ paddingTop: '1vh', paddingBottom: 168 }}>
       {/* Content */}
       <div className="relative z-10 flex flex-col items-center">
         {/* Hairline separator */}
